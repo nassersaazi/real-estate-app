a71996a636b60693f76c0a325a385b36e3e082a1
nassersaazi <nassersnoop@gmail.com>
2023-11-04 15:01:27 +0300

    Feat: contact landlord functionality
    Add contact landlord functionality to the listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,6 +9,20 @@ export const test = (req, res) => {
   });
 };
 
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
